31754006630575cb1ec752870baff77839b36656
Rajavignesh <uvaraj.seerangan@treselle.com>
2017-06-23 17:36:53 +0530

    second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String arg[]) {
-		System.out.println("Hello To Jenkins .............");
+		System.out.println("Hello is working fine in Jenkins .............");
 	}
 }
